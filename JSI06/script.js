1f703a3adaff3db45000d9a2c89ed042fd6c016e
vinhsenpai <quangvinhdo21@gmail.com>
2022-03-20 10:21:59 +0700

    code

---
+++ JSI06/script.js
@@ -0,0 +1,47 @@
+//function
+ 
+//call function
+//function ko tham so
+//function co tham so
+//function cos gia tri tra ve , function ko tra ve gia tri
+
+//arrow function
+// const logger= ()=>{
+//     console.log("hello")
+// }
+// logger();
+
+//objetct
+// const abc={
+//     name:'vinh',
+//     lastname:'Do',
+//     age:16,
+//     'nghenghiep':'hcsinh',
+// }
+// console.log(abc.name);
+// console.log(abc['nghenghiep'])
+
+//Destructuring
+// const {
+//     name:firstName,lastname,age,
+// }=abc;
+// console.log(firstName,lastname,age);
+
+// let name="vinh";
+// let age=16;
+
+// let person={
+//     name,
+//     age,
+// };
+// console.log(person)
+
+//Array
+// let number=[1,2,3,4,5];
+// let length=number.length;
+// console.log(number[length-1]);
+
+//module:import , export , export default;
+
+import{sum} from'math.js';
+console.log('sum',sum(1,2))
